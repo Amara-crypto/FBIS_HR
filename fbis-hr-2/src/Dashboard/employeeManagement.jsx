d15aa5e2ee6f77e09841322ab486cb30980bfb6f
thomasAmara <thomastarawali@gmail.com>
2021-10-20 12:41:25 +0100

    employement page

---
+++ fbis-hr-2/src/Dashboard/employeeManagement.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import DashboardLayout from '../components/DashboardLayout'
+
+export default function employeeManagement() {
+  return <DashboardLayout>employee</DashboardLayout>
+}

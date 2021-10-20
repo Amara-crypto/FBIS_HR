86a1107b15b5b0b6cedb5df1c913847d99ed8d54
thomasAmara <thomastarawali@gmail.com>
2021-10-20 12:44:51 +0100

    contact page

---
+++ fbis-hr-2/src/Dashboard/contracts.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import DashboardLayout from '../components/DashboardLayout'
+
+export default function contracts() {
+  return <DashboardLayout>Contacts</DashboardLayout>
+}

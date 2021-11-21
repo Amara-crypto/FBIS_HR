dd5da3413be7a28596e56024f6d073fee6e6327f
thomasAmara <thomastarawali@gmail.com>
2021-11-21 20:25:53 +0100

    Global Error handlers

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,9 @@ const dotenv = require ('dotenv')
 const connectDB = require('./config/db')
 const cors = require ('cors')
 const morgan = require ('morgan')
-const http = require('http')
 
 const AppError = require('./utils/appError')
+const globalErrorHandler = require('./controllers/errorController')
 const userRouter = require('./routes/userRoute')
 
 
@@ -16,37 +16,34 @@ connectDB()
 const corsOptions  = {
     'credentials': true,
     'origin': 'http://localhost:3000', 
-    'methods': ['GET','POST','DELETE','PUT','PATCH'],
-    'allowedHeaders': [ 'application/json', 'Accept', 'Access-Control-Allow-Headers', 'content-type',  'Origin', 'Content-Type', 'Authorization'],
-    'preflightContinue': false,   
+    // 'methods': ['GET','POST','DELETE','PUT','PATCH'],
+    // 'allowedHeaders': [ 'application/json', 'Accept', 'Access-Control-Allow-Headers', 'content-type',  'Origin', 'Content-Type', 'Authorization'], 
 }
 
 const app = express()
-// const app = http.createServer(function(req, res){
-//     res.setHeader('Content-Type', 'application/json')
-//     res.end
-// })
-// app.accepts('application/json')
+
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
-// app.use( function (req, res) {
-//     res.setHeader('Content-Type', 'Application/json')
-// })
 app.use(cors(corsOptions))
-app.use(morgan('combined'))
+app.use(morgan('dev'))
 
 
-
-//ROUTES
-// app.all( '/api/', function( req, res, next) {
+// app.use((req,res,next) => {
 //     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-//     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE")
+//     res.header(
+//         "Access-Control-Allow-Headers",
+//         "Origin, x-requested-With, Content-Type, Accept, Authorization"
+//     )
 //     res.header("Access-Control-Allow-Credentials", "true")
-//     res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept, X-Access-Token,X-Key")
-
-//     res.contentType('application/json')
-
+//     res.contentType("application/json")
+//     res.setHeader("Content-Type", "application/json")
+ 
+//     if (req.method === "OPTIONS") {
+//         res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET")
+//         return res.status(200).json({})
+//     }
 //     next()
+ 
 // })
 
 // app.use('/api/v1/users', function(req, res) {
@@ -59,10 +56,11 @@ app.use(morgan('combined'))
 
 app.use('/api/v1/users', userRouter)
 
-// app.all('*', (req,res,next) => {
-//     next(new AppError(`can't find ${req.originalUrl} on this server!`, 404))
-// })
+app.all('*', (req,res,next) => {
+    next(new AppError(`can't find ${req.originalUrl} on this server!`, 404))
+})
 
+app.use(globalErrorHandler)
 
 const port = process.env.PORT || 5000
 

d8b04bbab880c8ce49911e948a8f310c69d6b8d4
thomasAmara <thomastarawali@gmail.com>
2021-11-26 20:25:11 +0100

    token function added

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -9,7 +9,31 @@ const asyncHandler = require('express-async-handler')
 
 const signToken = (id) => {
     return jwt.sign({id}, process.env.JWT_SECRET, {
-        expiresIn: '3d'
+        expiresIn: '72h'
+    })
+}
+
+const createSendToken = ( user, statusCode, res) => {
+    const token = signToken(user._id)
+
+    const cookieOptions = { 
+        expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 100),
+        httpOnly: true
+    }
+    if ( process.env.NODE_ENV === 'production') cookieOptions.secure = true
+
+    res.cookie('jwt', token, cookieOptions)
+
+    user.password = undefined
+
+    res.status(statusCode).json({
+        status: 'success',
+        access_token:token,
+        message: 'success',
+        success: true,
+        data: {
+            user,  
+        }
     })
 }
 
@@ -38,24 +62,6 @@ exports.signup = asyncHandler( async(req, res, next) => {
     
 })
 
-exports.authLogin = asyncHandler (async (req, res) => {
-    // const token = signToken(user._id)
-
-        const { email, password } = req.body;    
-        const user = await User.findOne({ email })
-
-        if ( user && ( user.correctPassword(password))) {
-            res.json({
-                _id: user._id,
-                name: user.name,
-                email: user.email,
-                token: signToken(user._id)
-            })
-        } else {
-            res.status(401)
-            throw new Error ('Invalid password')
-        } 
-})
 
 exports.login = catchAsync( async(req, res, next) => {
     // console.log(req.body)
@@ -72,33 +78,38 @@ exports.login = catchAsync( async(req, res, next) => {
         return next ( new AppError('Incorrect email or Password', 401))
     }
 
-    const token = signToken(user._id)
-    res.status(200).json({
-        status: 'success',
-        message: 'Success',
-        data:{
-            access_token:token,
-            name:user.name,
-            email:user.email,
-            role: user.role
-            // user
-        }, 
-        success: true,
-    })
+    // const token = signToken(user._id)
+    // res.status(200).json({
+    //     status: 'success',
+    //     message: 'Success',
+    //     data:{
+    //         access_token:token,
+    //         name:user.name,
+    //         email:user.email,
+    //         role: user.role,
+           
+    //         // user
+    //     }, 
+    //     success: true,
+    // })
+
+    createSendToken(user, 200, res)
 })
 
 exports.protect = catchAsync( async (req, res, next) => {
-    let token 
+    let access_token 
     if (req.headers.authorization &&
         req.headers.authorization.startsWith('Bearer'))
-    {
-        token = req.headers.authorization.split(' ')[1]
-    }
-    if (!token) {
-        return next(new AppError('You are not logged in! Please log in to get access', 401))
-    }
+       
+        {
+            access_token = req.headers.authorization.split(' ')[1]
+        }
 
-    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET)
+        if (!access_token) {
+            return next(new AppError('You are not logged in! Please log in to get access', 401))
+        }
+    console.log('this token is', access_token)
+    const decoded = await promisify(jwt.verify)(access_token, process.env.JWT_SECRET)
 
     const currentUser = await User.findById(decoded.id)
     if (!currentUser) {
@@ -204,12 +215,5 @@ exports.updatePassword = catchAsync ( async (req, res, next) => {
         user.passwordConfirm = req.body.passwordConfirm
         await user.save()
 
-        const token = signToken(user._id)
-        res.status(200).json({
-            status:'success',
-            token,
-            data: {
-                user
-            }
-        })
+        createSendToken(user, 200, res)
 })

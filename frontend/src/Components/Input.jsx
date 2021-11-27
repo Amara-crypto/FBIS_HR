14d31ee0992995a227409c925e566f0ea065f8d0
thomasAmara <thomastarawali@gmail.com>
2021-11-27 13:22:42 +0100

    Change Id names

--- frontend/src/Components/Input.jsx
+++ frontend/src/Components/Input.jsx
@@ -3,14 +3,21 @@ export const PASS = [
     title: 'Current Password',
     placeholder: 'Enter Current password',
     tag: 'name',
-    id: 'currentPass',
+    id: 'passwordCurrent',
     type: 'input',
   },
   {
     title: 'New Password',
     placeholder: 'Enter new password',
     tag: 'username',
-    id: 'pin',
+    id: 'password',
+    type: 'input',
+  },
+  {
+    title: 'Confirm New Password',
+    placeholder: 'Confirm new password',
+    tag: 'username',
+    id: 'passwordConfirm',
     type: 'input',
   },
 ]

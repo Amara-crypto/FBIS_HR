e1e71f2f32566331f398a8f895556b1b4fb25be3
thomasAmara <thomastarawali@gmail.com>
2021-11-28 18:15:51 +0100

    resetPassword

--- frontend/src/Onboarding/ForgotPassword.jsx
+++ frontend/src/Onboarding/ForgotPassword.jsx
@@ -1,9 +1,82 @@
 import React from 'react'
 import { Input, InputGroup, Button } from '@chakra-ui/react'
 import { Link } from 'react-router-dom'
+import { HandleAllRequest } from '../Api/fetchApi'
 import '../Styles/Onboarding.scss'
+import { useMutation } from 'react-query'
+import { useToast } from '@chakra-ui/react'
+import { passwordResetSuccess, passwordResetError } from '../reducer/Action'
+import { useDispatch, useSelector } from 'react-redux'
+import { useHistory } from 'react-router'
+import { useEffect } from 'react'
 
 function ForgotPassword() {
+  const [inputChange, setInput] = React.useState('')
+
+  let history = useHistory()
+  const toast = useToast()
+  const dispatch = useDispatch()
+
+  const { isReset } = useSelector((state) => state.ResetPasswordReducer)
+
+  const handleInputChange = (e) => {
+    // setInput(e.target.value)
+    setInput({
+      ...inputChange,
+      [e.currentTarget.id]: e.currentTarget.value,
+    })
+  }
+
+  const resetMutation = useMutation(
+    (data) =>
+      HandleAllRequest('/api/v1/users/forgotPassword', 'post', '', data),
+    {
+      onSuccess: (data) => {
+        let s = data.success
+        let m = data.message
+        let d = data
+        if (s) {
+          dispatch(passwordResetSuccess(d))
+          toast({
+            position: 'top-right',
+            title: 'Success.',
+            description: 'm',
+            status: 'success',
+            duration: 9000,
+            isClosable: true,
+          })
+        } else {
+          dispatch(passwordResetError())
+          toast({
+            position: 'top-right',
+            title: 'Error. message',
+            description: m,
+            status: 'error',
+            duration: 9000,
+            isClosable: true,
+          })
+        }
+      },
+      onError: (error) => {
+        dispatch(passwordResetError())
+        toast({
+          position: 'top-left',
+          title: 'Error. message',
+          description: `${error}`,
+          status: 'error',
+          duration: 9000,
+          isClosable: true,
+        })
+      },
+    }
+  )
+
+  useEffect(() => {
+    if (isReset === true) {
+      history.replace('/resetPassword')
+    }
+  }, [history, isReset])
+
   return (
     <div className='onBoardingContainer'>
       <div className='onBoardingGroup'>
@@ -12,9 +85,17 @@ function ForgotPassword() {
           Please provide your email below to reset your Password.
         </div>
         <div className='inputs'>
-          <InputGroup className='email'>
-            <Input placeholder='Enter email' />
-          </InputGroup>
+          {Reset.map((data, index) => (
+            <InputGroup key={index} className='email'>
+              <Input
+                type={data.id}
+                placeholder={data.placeholder}
+                onChange={(e) => handleInputChange(e)}
+                value={inputChange.email}
+                id={data.id}
+              />
+            </InputGroup>
+          ))}
 
           <Button
             width='100%'
@@ -29,6 +110,11 @@ function ForgotPassword() {
               outline: 'none',
               boxShadow: 'none',
             }}
+            onClick={() => {
+              resetMutation.mutate(inputChange)
+            }}
+
+            // isLoading={resetMutation.isLoading}
           >
             Continue
           </Button>
@@ -43,5 +129,15 @@ function ForgotPassword() {
     </div>
   )
 }
+
+const Reset = [
+  {
+    title: 'Current Email',
+    placeholder: 'Enter Email',
+    tag: 'name',
+    id: 'email',
+    type: 'email',
+  },
+]
 
 export default ForgotPassword
